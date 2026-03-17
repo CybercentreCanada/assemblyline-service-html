7150765063ec6b8e9ef0c1295a32cca85402dd02
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2026-03-17 18:11:04 +0000

    Add sections to display href and src attributes

--- html_/html_.py
+++ html_/html_.py
@@ -1,16 +1,20 @@
-"""This Assemblyline service analyzes HTML documents."""
+"""Static HTML analysis."""
 
-from assemblyline.common import forge
+import bs4
 from assemblyline_v4_service.common.base import ServiceBase
 from assemblyline_v4_service.common.request import ServiceRequest
-from assemblyline_v4_service.common.result import Result
+from assemblyline_v4_service.common.result import ResultSection
 
 
 class HTML(ServiceBase):
-    """This Assemblyline service analyzes HTML documents."""
+    """Assemblyline service for static HTML analysis."""
 
     def execute(self, request: ServiceRequest):
         """Run the service."""
-
-        result = Result()
-        request.result = result
+        soup = bs4.BeautifulSoup(request.file_contents, features="lxml")
+        hrefs = soup.find_all(href=True)
+        if hrefs:
+            request.result.add_section(ResultSection("href attributes", [href.get("href", "") for href in hrefs]))
+        srcs = soup.find_all(src=True)
+        if srcs:
+            request.result.add_section(ResultSection("src attributes", [src.get("src", "") for src in srcs]))
