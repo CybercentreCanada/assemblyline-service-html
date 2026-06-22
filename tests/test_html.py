0b2c056123f66e57509b8686840c4c4d2554de05
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2026-06-22 16:40:19 +0000

    Check for unnecessary HTML entities

--- tests/test_html.py
+++ tests/test_html.py
@@ -66,3 +66,15 @@ import html_.html_
 )
 def test_tag_uris(uris: list[str], tags: dict[str, list[str]]) -> None:
     assert html_.html_.tag_urls(uris) == tags
+
+
+@pytest.mark.parametrize(
+    ("data", "section"),
+    [
+        (b"", False),
+        (b"P&#97;&#115;&#115;&#119;&#111;&#114;d", True),
+        (b"&#47;&#x2F;&#58;&#x3A;&#64;&#x40;&#91;&#x5B;&#96;&#x60;&#123;&#x7B;", False),
+    ],
+)
+def test_check_html_entities(data: bytes, section: bool) -> None:
+    assert bool(html_.html_.check_html_entities(data)) == section
